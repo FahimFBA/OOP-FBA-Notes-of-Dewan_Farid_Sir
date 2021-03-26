e240ceceb2e1d73b7390a7b4a122e79c37c45160
FahimFBA <fahimbinamin@gmail.com>
2021-03-26 18:42:41 +0600

    Blocks in java

---
+++ 0008 - Blocks/BlockTest.java
@@ -0,0 +1,24 @@
+public class BlockTest{
+    public String info;
+
+
+    public BlockTest() {
+        info = "Constructors: Executed in 2nd step";
+        System.out.println(info);
+    }
+
+    {
+        info = "Block: Executed in 1st step";
+        System.out.println(info);
+    }
+
+    public void show(){
+        info = "Method: Executed in 3rd step";
+        System.out.println(info);
+    }
+
+    public static void main(String[] args){
+        BlockTest bt = new BlockTest();
+        bt.show();
+    }
+}
