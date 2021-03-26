36e227fc5a9334e57e7bd5ac90d8055d3380a590
FahimFBA <fahimbinamin@gmail.com>
2021-03-26 22:14:22 +0600

    If else statements in java

---
+++ 0012 - If-else Statement/IfTest.java
@@ -0,0 +1,16 @@
+public class IfTest{
+    public static void main(String[] args){
+        int x = (int) (Math.random() * 100);
+        System.out.println(x);
+        // if else statement
+
+        if (x>100)
+        {
+            System.out.println("White");
+        }
+        else
+        {
+            System.out.println("Black");
+        }
+    }
+}
