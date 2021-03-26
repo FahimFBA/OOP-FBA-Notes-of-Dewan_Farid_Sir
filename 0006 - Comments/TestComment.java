abca40b136fa12acf06bc9f1e4405a8c5e5a328d
FahimFBA <fahimbinamin@gmail.com>
2021-03-26 18:32:35 +0600

    3 types of comments in java

---
+++ 0006 - Comments/TestComment.java
@@ -0,0 +1,14 @@
+public class TestComment{
+    // comment on one line
+
+    public static void main(String[] args){
+        /* comment on one
+        or more lines
+        */
+        System.out.println("Comments in Java");
+    }
+
+    /** documentation comment
+     *  can also span one or more lines
+     */
+}
