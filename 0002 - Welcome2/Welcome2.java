338922382bc1ae1b3e318cb01e01b0017e5eb806
FahimFBA <fahimbinamin@gmail.com>
2021-03-26 14:51:15 +0600

    created a new class and an object of the new class

---
+++ 0002 - Welcome2/Welcome2.java
@@ -0,0 +1,12 @@
+public class Welcome2 { 
+    public static void main(String[] args) { // main method
+        Hello hello = new Hello(); // creating an object named hello of Hello class
+        hello.display(); // calling the display method of the hello object of Hello class 
+    }
+}
+
+class Hello {
+    public void display() { // method of the class
+        System.out.println("Welcome to Java World");
+    }
+}
