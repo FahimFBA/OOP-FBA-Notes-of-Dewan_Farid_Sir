4a0eaaf3bb4b6af0cbb92f5d2e48d8aba11d80db
FahimFBA <fahimbinamin@gmail.com>
2021-03-26 21:53:58 +0600

    String Concatenation technique with + sign

---
+++ 0010 - String Concatenation with +/StringConTest.java
@@ -0,0 +1,8 @@
+public class StringConTest{
+    public static void main(String[] args){
+        String salutation = "Mr. ";
+        String name = "Dewan " + "Md. " + "Farid" ;
+        String title = salutation + name;
+        System.out.println(title);
+    }
+}
