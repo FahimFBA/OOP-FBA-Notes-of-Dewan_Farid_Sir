91bfbda7c3ca410313802075eb035e3bf79e00dc
FahimFBA <fahimbinamin@gmail.com>
2021-03-26 18:56:41 +0600

    "this" reference in java

---
+++ 0009 - The this Reference/AddNumbers.java
@@ -0,0 +1,19 @@
+public class AddNumbers{
+    private int num1, num2, result;
+
+
+    public AddNumbers(int num1, int num2){
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public void add() {
+        result = num1 + num2;
+        System.out.println("Result is: " + result);
+    }
+    public static void main(String[] args){
+        AddNumbers addnum = new AddNumbers(10, 20);
+        addnum.add();
+    }
+
+}
