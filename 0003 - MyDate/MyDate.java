aa8bc72179c1786e8921a9e28ad433e44bda976c
FahimFBA <fahimbinamin@gmail.com>
2021-03-26 15:18:43 +0600

    Creating an object and calling that through the main method

---
+++ 0003 - MyDate/MyDate.java
@@ -0,0 +1,25 @@
+public class MyDate {
+	private int day;
+	private int month;
+	private int year;
+
+	public MyDate() // Constructor
+	{
+		day = 1;
+		month = 6;
+		year = 1979;
+	}
+
+	public void showDate() // method
+	{
+		System.out.println("Day: " + day);
+		System.out.println("Month: " + month);
+		System.out.println("Year: " + year);
+
+	}
+
+	public static void main(String[] args){ // main method
+		MyDate date = new MyDate(); // creating an object of the class
+		date.showDate(); // calling the showDate method of the object named date
+	}
+}
