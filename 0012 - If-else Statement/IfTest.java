88e791478b2a06b3492b5244ad8c785fc792ab10
FahimFBA <fahimbinamin@gmail.com>
2021-03-27 21:12:54 +0600

    If else statements in Java

--- 0012 - If-else Statement/IfTest.java
+++ 0012 - If-else Statement/IfTest.java
@@ -2,7 +2,7 @@ public class IfTest{
     public static void main(String[] args){
         int x = (int) (Math.random() * 100);
         System.out.println(x);
-        // if else statement
+        // if else statement 
 
         if (x>100)
         {
