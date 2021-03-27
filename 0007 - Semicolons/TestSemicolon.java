6252016ce659fa306aa33b49ab9bd02408a45f81
FahimFBA <fahimbinamin@gmail.com>
2021-03-27 21:06:25 +0600

    Semicolon in Java

--- 0007 - Semicolons/TestSemicolon.java
+++ 0007 - Semicolons/TestSemicolon.java
@@ -1,7 +1,7 @@
 public class TestSemicolon{
     public static void main(String[] args){
         int a = 10, b = 20, c;
-        c = a + b;
+        c = a + b; 
         System.out.println(c); 
     }
 }
