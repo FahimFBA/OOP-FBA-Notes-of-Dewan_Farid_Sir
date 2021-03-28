97991a2b9e769ba4b895ac452a6541f03836646e
FahimFBA <fahimbinamin@gmail.com>
2021-03-28 18:09:11 +0600

    Do While loop in java

---
+++ 0019 - Do-while Loop/Loop_do.java
@@ -0,0 +1,16 @@
+public class Loop_do{
+    public static void main(String[] args){
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while(i <= 50);
+    }
+}
+
+// Syntax:
+// initialization;
+// do {
+//     statements;
+//     increment/decrement;
+// } while(condition);
