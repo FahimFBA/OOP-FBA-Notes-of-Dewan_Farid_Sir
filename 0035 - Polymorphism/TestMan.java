940f232bb70aad30bcb90e514f50316bec54ad11
FahimFBA <fahimbinamin@gmail.com>
2021-03-29 16:25:05 +0600

    create obj of parentclass through the cons of child class

---
+++ 0035 - Polymorphism/TestMan.java
@@ -0,0 +1,18 @@
+class Man {
+    public void fly() {
+        System.out.println("Man can not fly");
+    }
+}
+
+class SuperMan extends Man {
+    public void fly(){
+        System.out.println("Superman can fly");
+    }
+}
+
+public class TestMan{
+    public static void main(String[] args){
+        Man m = new SuperMan(); // polymorphism
+        m.fly();
+    }
+}
