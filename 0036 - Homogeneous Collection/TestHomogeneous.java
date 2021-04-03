6a431ce319d251eabe58b610286159b2c03e6ab3
FahimFBA <fahimbinamin@gmail.com>
2021-04-03 19:53:42 +0600

    Homogeneous Collection

---
+++ 0036 - Homogeneous Collection/TestHomogeneous.java
@@ -0,0 +1,9 @@
+public static TestHomogeneous {
+  public static void main(String[] args){
+    TestHomogeneous[] arr = new TestHomogeneous[3];
+
+    arr[0] = new TestHomogeneous();
+    arr[1] = new TestHomogeneous();
+    arr[2] = new TestHomogeneous();
+  }
+}
