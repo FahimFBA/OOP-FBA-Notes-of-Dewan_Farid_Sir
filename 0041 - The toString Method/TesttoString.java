d3fd48ce88508b13b9386a66a816beb70abc164b
FahimFBA <fahimbinamin@gmail.com>
2021-04-10 18:40:36 +0600

    This method of Object class converts an object to a String representation

---
+++ 0041 - The toString Method/TesttoString.java
@@ -0,0 +1,8 @@
+public class TesttoString {
+    public static void main(String[] args) {
+        TesttoString now = new TesttoString();
+        System.out.println(now.toString());
+        // is equavalent to :
+        System.out.println(now.toString());
+    }
+}
