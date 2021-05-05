70e079732eb561953d632aee1a490ed52980b56c
Md. Fahim Bin Amin <fahimbinamin@gmail.com>
2021-05-05 21:23:39 +0600

    we can provide zero or more command line arguments of String type from a terminal window

---
+++ 0054 - Command-Line Arguments/CommandLine.java
@@ -0,0 +1,7 @@
+public class CommandLine {
+    public static void main(String[] args){
+        for(int i = 0 ; i < args.length ; i++){
+            System.out.println("Value of args in " + i + " index is: " + args[i]);
+        }
+    }
+}
